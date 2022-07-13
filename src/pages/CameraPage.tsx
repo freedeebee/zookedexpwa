bb3512b5daaa2bb385aac29b4a7e6001a1f99e90
Dennis Schad <dennis.schad1991@gmail.com>
2022-07-13 17:32:29 +0200

    camera stuff

--- src/pages/CameraPage.tsx
+++ src/pages/CameraPage.tsx
@@ -1,19 +1,39 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { FormEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function CameraPage() {
 
-  return (
-    <div>
-          <input
-            accept="image/*"
-            id="icon-button-file"
-            type="file"
-            capture="environment"
-            onChange={(e) => console.log(e.target)}
-          />
-          
-    </div>
-  );
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        // @ts-ignore
+        const file = e.target.files[0];
+        const formData = new FormData();
+        formData.append('image', file)
+
+        axios
+            .post("/upload", formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            })
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+        
+    return (
+        <form>
+            <input
+                accept="image/*"
+                id="icon-button-file"
+                type="file"
+                capture="environment"
+                onChange={(e) => handleFileChange(e)}
+            />
+        </form>
+    );
 }
 export default CameraPage;
