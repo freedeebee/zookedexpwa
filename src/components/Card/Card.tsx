b735389344a7100b8ce46f265fe8acd10500c43d
JasyWi <jasmin-wieblingen@hotmail.de>
2022-07-13 19:14:45 +0200

    Added prettier

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,27 +1,45 @@
-import { CardProps } from "./types"
-import React from 'react';
-import { format } from 'date-fns'
-import { AnimalCard,ImageWrapper, InformationContainer, AnimalName, AnimalImage, Info, TextWrapper} from './styles'
-import {BiTimeFive} from 'react-icons/bi'
-import {GoLocation} from 'react-icons/go'
-import {AiOutlineEye} from 'react-icons/ai'
+import { CardProps } from "./types";
+import React from "react";
+import { format } from "date-fns";
+import {
+  AnimalCard,
+  ImageWrapper,
+  InformationContainer,
+  AnimalName,
+  AnimalImage,
+  Info,
+  TextWrapper,
+} from "./styles";
+import { BiTimeFive } from "react-icons/bi";
+import { GoLocation } from "react-icons/go";
+import { AiOutlineEye } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
 
 export default function Card(props: CardProps) {
-   let navigate = useNavigate();
-   
-    
-    return (
-      <AnimalCard onClick={() => navigate('/detail')} className="card">
-         <ImageWrapper>
-            <AnimalName className="card--title">{props.name}</AnimalName>
-            <AnimalImage src={props.src} className="card--image"></AnimalImage>
-         </ImageWrapper>
-         <InformationContainer className="card--infos">
-            <Info><BiTimeFive /><TextWrapper>{format(props.date, 'dd.MM.yyyy').toString()}</TextWrapper></Info>
-            <Info><GoLocation /><TextWrapper>{props.location}</TextWrapper></Info>
-            <Info><AiOutlineEye /><TextWrapper>{props.views}</TextWrapper></Info>
-         </InformationContainer>
-      </AnimalCard>
-    )
+  let navigate = useNavigate();
+
+  return (
+    <AnimalCard onClick={() => navigate("/detail")} className='card'>
+      <ImageWrapper>
+        <AnimalName className='card--title'>{props.name}</AnimalName>
+        <AnimalImage src={props.src} className='card--image'></AnimalImage>
+      </ImageWrapper>
+      <InformationContainer className='card--infos'>
+        <Info>
+          <BiTimeFive />
+          <TextWrapper>
+            {format(props.date, "dd.MM.yyyy").toString()}
+          </TextWrapper>
+        </Info>
+        <Info>
+          <GoLocation />
+          <TextWrapper>{props.location}</TextWrapper>
+        </Info>
+        <Info>
+          <AiOutlineEye />
+          <TextWrapper>{props.views}</TextWrapper>
+        </Info>
+      </InformationContainer>
+    </AnimalCard>
+  );
 }
