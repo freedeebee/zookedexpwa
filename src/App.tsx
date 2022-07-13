83d759095a1e842fee194c6f72eb3af2e2d02d86
Dennis Benert <be.nerdcreation@gmail.com>
2022-07-13 17:50:30 +0200

    detail page are now dynamic init

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,14 @@ import DetailPage from './pages/DetailPage/DetailPage';
 import CameraPage from './pages/CameraPage';
 import Homepage from './pages/Homepage';
 import MapPage from './pages/MapPage';
+import { Lorem } from './utils/Lorem';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<ContentContainer />}>
-          <Route path='detail' element={<DetailPage />} />
+          <Route path='detail' element={<DetailPage imageSrc={'../../assets/images/photo-1614027164847-1b28cfe1df60.jpg'} animalName={'Katze'} location={'Ghana, Accra'} date={'01.01.1970'} text={Lorem}  />} />
           <Route path='camera' element={<CameraPage />} />
           <Route path='' element={<Homepage />} />
           <Route path='map' element={<MapPage />} />
