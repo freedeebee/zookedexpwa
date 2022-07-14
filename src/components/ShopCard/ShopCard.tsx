b4e5f17dfc55de870646d421eca5fd056d9a365e
JasyWi <jasmin-wieblingen@hotmail.de>
2022-07-14 19:14:11 +0200

    shop updated

--- src/components/ShopCard/ShopCard.tsx
+++ src/components/ShopCard/ShopCard.tsx
@@ -5,8 +5,13 @@ import { ShopCardProps } from "./types";
 const ShopCard = (props: ShopCardProps) => {
   return (
     <Container>
-      <Image src={props.imageSrc} />
-      <p>{props.id}</p>
+      <ImageWrapper>
+        <Name className='card--title'>{props.name}</Name>
+        <Points>{props.price}</Points>
+        <Image src={props.imageSrc} />
+      </ImageWrapper>
+
+      <p>{props.previewText}</p>
     </Container>
   );
 };
@@ -26,4 +31,37 @@ const Image = styled.img`
   width: 200px;
   display: block;
   object-fit: cover;
+`;
+
+const Name = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 2em;
+  background-color: ${({ theme }) => theme.colors.secondary};
+  padding: 4px 12px;
+  color: ${({ theme }) => theme.colors.black};
+  border-radius: 10px 0 10px 0;
+`;
+
+const Points = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 2em;
+  background-color: ${({ theme }) => theme.colors.secondary};
+  padding: 4px 12px;
+  color: ${({ theme }) => theme.colors.black};
+  border-radius: 0 10px 0 10px;
+`;
+
+export const ImageWrapper = styled.div`
+  object-fit: fill;
+  position: relative;
+  display: flex;
+  width: 100%;
+
+  &:before {
+    background: rgba(0, 255, 255, 0.5);
+  }
 `;
