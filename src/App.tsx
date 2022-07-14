f1d300104499c9cc03a98565e4da968686e93088
JasyWi <jasmin-wieblingen@hotmail.de>
2022-07-14 15:34:59 +0200

    Added DonationPage

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,20 @@ import Homepage from "./pages/Homepage";
 import MapPage from "./pages/MapPage";
 import Profile from "./components/Profile/Profile";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import DonationPage from "./pages/DonationPage/DonationPage";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Routes>
-        <Route path="/" element={<ContentContainer />}>
-          <Route path="detail/:id" element={<DetailPage />} />
-          <Route path="" element={<Homepage />} />
-          <Route path="map" element={<MapPage />} />
-          <Route path="profile" element={<Profile />} />
+        <Route path='/' element={<ContentContainer />}>
+          <Route path='detail/:id' element={<DetailPage />} />
+          <Route path='donation' element={<DonationPage />} />
+          <Route path='' element={<Homepage />} />
+          <Route path='map' element={<MapPage />} />
+          <Route path='profile' element={<Profile />} />
         </Route>
-        <Route path="login" element={<LoginPage />} />
+        <Route path='login' element={<LoginPage />} />
       </Routes>
     </div>
   );
