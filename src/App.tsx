d225d36d8c0e987877da15fa9075f3b7ff81f955
Dennis Benert <be.nerdcreation@gmail.com>
2022-07-14 11:13:52 +0200

    login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import BottomNavigation from "./components/BottomNavigation/BottomNavigation";
 import ContentContainer from "./components/Containers/ContentContainer";
 import DetailPage from "./pages/DetailPage/DetailPage";
 import Homepage from "./pages/Homepage";
 import MapPage from "./pages/MapPage";
-import { Lorem } from "./utils/Lorem";
 import Profile from "./components/Profile/Profile";
+import LoginPage from "./pages/LoginPage/LoginPage";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Routes>
-        <Route path='/' element={<ContentContainer />}>
-          <Route path='detail/:id' element={<DetailPage />} />
-          <Route path='' element={<Homepage />} />
-          <Route path='map' element={<MapPage />} />
-          <Route path='profile' element={<Profile />} />
+        <Route path="/" element={<ContentContainer />}>
+          <Route path="detail/:id" element={<DetailPage />} />
+          <Route path="" element={<Homepage />} />
+          <Route path="map" element={<MapPage />} />
+          <Route path="profile" element={<Profile />} />
         </Route>
+        <Route path="login" element={<LoginPage />} />
       </Routes>
-      <BottomNavigation />
     </div>
   );
 }
--- src/components/DetailView/InformationContent.tsx
+++ src/components/DetailView/InformationContent.tsx
@@ -1,38 +1,34 @@
-import React from 'react'
-import styled from 'styled-components'
-import { AnimalInformationProps } from './types'
+import React from "react";
+import styled from "styled-components";
+import { AnimalInformationProps } from "./types";
 
 const InformationContent = (props: AnimalInformationProps) => {
-  return (
-    <Scroller>
-      {props.text}
-    </Scroller>
-  )
-}
+  return <Scroller>{props.text}</Scroller>;
+};
 
-export default InformationContent
+export default InformationContent;
 
-const Scroller= styled.div`
-overflow-y: scroll;
-height: calc(100% - 300px);
-padding: 8px 20px 64px 20px;
-::-webkit-scrollbar {
-  width: 8px;
-}
+const Scroller = styled.div`
+  overflow-y: scroll;
+  height: calc(100% - 300px);
+  padding: 8px 20px 64px 20px;
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
 
-/* Track */
-::-webkit-scrollbar-track {
-  border-radius: 10px;
-}
- 
-/* Handle */
-::-webkit-scrollbar-thumb {
-  background: ${({theme})=> theme.colors.secondary}; 
-  border-radius: 10px;
-}
+  /* Track */
+  ::-webkit-scrollbar-track {
+    border-radius: 10px;
+  }
 
-/* Handle on hover */
-::-webkit-scrollbar-thumb:hover {
-  background: ${({theme})=> theme.colors.secondary} 
-}
-`
+  /* Handle */
+  ::-webkit-scrollbar-thumb {
+    background: ${({ theme }) => theme.colors.secondary};
+    border-radius: 10px;
+  }
+
+  /* Handle on hover */
+  ::-webkit-scrollbar-thumb:hover {
+    background: ${({ theme }) => theme.colors.secondary};
+  }
+`;
