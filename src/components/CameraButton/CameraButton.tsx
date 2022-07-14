372f6f172790ccf7f9746e3e9b3097c8a86f35e3
doeig <dominik.eigner@gmx.de>
2022-07-14 13:30:09 +0200

    add geopos

--- src/components/CameraButton/CameraButton.tsx
+++ src/components/CameraButton/CameraButton.tsx
@@ -3,12 +3,18 @@ import { StyledCameraButton } from "./styles";
 import { BsCameraFill } from 'react-icons/bs'
 
 function CameraButton() {
+    
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
         // @ts-ignore
         const file = e.target.files[0];
         const formData = new FormData();
         formData.append('image', file)
 
+        navigator.geolocation.getCurrentPosition(position => {
+            formData.append("latitude", String(position.coords.latitude));
+            formData.append("longitude", String(position.coords.longitude));
+        });
+        
         axios
             .post("/upload", formData, {
                 headers: {
